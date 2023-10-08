24a8264a8a8080cda710c5570ba08fb77f9be768
Danil Vorontsov <rojerr@MacBook-Air-Danil.local>
2023-10-08 20:38:57 +0600

    init

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+import { formatDate } from '../../helpers/formatDate';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>NEWS</h1>
+      <p className={styles.date}>{formatDate(new Date())}</p>
+    </header>
+  );
+};
