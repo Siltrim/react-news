2c6eba7343c6d3baf63bc4d907b9869c81ad6729
Siltrim <rojerr410@gmail.com>
2023-11-29 13:12:29 +0600

    added typescript

--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-
 import styles from './styles.module.css';
 import Search from '../Search/Search';
 import Categories from '../Categories/Categories';
 import { useFetch } from '../../helpers/hooks/useFetch';
 import { getCategories } from '../../api/apiNews';
 import Slider from '../Slider/Slider';
+import { CategoriesApiResponse, IFilters } from '../../interfaces';
 
-const NewsFilters = ({ filters, changeFilter }) => {
-  const { data: dataCategories } = useFetch(getCategories);
+interface Props {
+  filters: IFilters;
+  changeFilter: (key: string, value: string | null | number) => void;
+}
+
+const NewsFilters = ({ filters, changeFilter }: Props) => {
+  const { data: dataCategories } = useFetch<CategoriesApiResponse, null>(getCategories);
 
   return (
     <div className={styles.filters}>
